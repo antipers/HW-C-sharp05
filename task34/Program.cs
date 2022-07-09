a093d799e35b35b63ae38b0921cebacd9b4c93cb
antipers <antipers@gmail.com>
2022-07-09 21:41:44 +0300

    created repo, .gitignore, solved task34

---
+++ task34/Program.cs
@@ -0,0 +1,34 @@
+﻿// Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. 
+//Напишите программу, которая покажет количество чётных чисел в массиве.
+
+// [345, 897, 568, 234] -> 2
+
+int[] array = new int[35];
+
+void fillArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(100, 1000);
+        System.Console.Write($"{array[i]}, ");
+
+    }
+    System.Console.WriteLine("\b\b \b");
+}
+
+void findNumbers(int[] array)
+{int count=0;
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i]%2==0)
+        {
+            count++;
+        }
+    }
+System.Console.WriteLine(count);
+}
+
+fillArray(array);
+System.Console.WriteLine("=============================");
+findNumbers(array);
